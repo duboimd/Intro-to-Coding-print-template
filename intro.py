fe14349f5c753dfb024157690d422f80018fa31c
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-09-13 08:10:19 -0600

    Create intro.py
    Complete the assignment following directions in the comment.

---
+++ intro.py
@@ -0,0 +1,37 @@
+# 1. Print "Hello World!" to the console
+
+
+
+# 2. Print "Hello my name is ______!" to the console
+
+
+
+# 3. Print "I love python!" to the console
+
+
+
+# 4. Print your age to the console
+
+
+
+# 5. Print 5+5
+
+
+
+# 6. Print 4*3
+
+
+
+# 7. Print 50 - 40
+
+
+
+# 8. Print 100/5 and print 100//5. Make a comment explaining what the difference is.
+
+
+
+# 9. Print 2**4
+
+
+
+# 10. Print 6*3-4+8//2
